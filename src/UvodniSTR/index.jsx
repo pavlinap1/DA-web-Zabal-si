658a9e69d3c744c67ce897f240d46b78b3d55df5
Pavlina Petyrkova <petyrkovapavlina@seznam.cz>
2021-05-27 22:07:46 +0200

    Přidán částečně fungující formulář

---
+++ src/UvodniSTR/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const UvodniSTR = () => {
+  return (
+    <>
+      <img />
+      <p>
+        Vítáme Vás na stránce Zabal si, která slouží k plánování dovolené či
+        jiné cesty.
+      </p>
+      <p>Na začátku si můžete vyplnit formulář o vaší cestě.</p>
+      <button>Vyplnit formulář</button>
+    </>
+  );
+};
+
+export default UvodniSTR;
+//https://www.codegrepper.com/code-examples/javascript/how+to+scroll+window+onclick+button+scrollbar+js
+
+//https://stackoverflow.com/questions/18071046/smooth-scroll-to-specific-div-on-click/18071231
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Formular from './Formular';
 import './style.css';
+import UvodniSTR from './UvodniSTR';
+import Vizitka from './Vizitka';
 
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Zabal si</h1>
-    </header>
-    <main>
-      <p>
-        Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí{' '}
-        <a href="https://www.npmjs.com/package/create-czechitas-app">
-          create-czechitas-app
-        </a>
-        .
-      </p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
-  </div>
-);
+const App = () => {
+  return (
+    <>
+      <UvodniSTR />
+      <Formular />
+      <Vizitka />
+    </>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
