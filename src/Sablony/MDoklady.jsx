05fb8d5f1162faf0a6e14afb70f4556ef3333ba9
Pavlina Petyrkova <petyrkovapavlina@seznam.cz>
2021-06-03 19:35:59 +0200

    Databáze-část moře

--- src/Sablony/MDoklady.jsx
+++ src/Sablony/MDoklady.jsx
@@ -1,33 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../db.js';
+import firebase from 'firebase/app';
+import './style.css';
 
 const MDoklady = () => {
   const [polozky, setPolozky] = useState([]);
 
   useEffect(() => {
-    const uklidPoSobe = db.collection('more-doklady').onSnapshot((snapshot) => {
-      setPolozky(
-        snapshot.docs.map((doc) => {
-          const data = doc.data();
-          data.id = doc.id;
-          return data;
-        }),
-      );
-    });
+    const uklidPoSobe = db
+      .collection('more-doklady')
+      .orderBy('datumVytvoreni')
+      .onSnapshot((snapshot) => {
+        setPolozky(
+          snapshot.docs.map((doc) => {
+            const data = doc.data();
+            data.id = doc.id;
+            return data;
+          }),
+        );
+      });
 
     return uklidPoSobe;
   }, []);
 
+  const [pridaniPolozky, setPridaniPolozky] = useState('');
+  const [openList, setOpenList] = useState(false);
+  const handleClick = () => {
+    setOpenList(!openList);
+  };
+
   return (
     <>
       <h1>Moře</h1>
-      <h3>Doklady</h3>
       <div>
-        <ul>
-          {polozky.map((polozka) => (
-            <li key={polozka.nazev}>{polozka.nazev}</li>
-          ))}
-        </ul>
+        <button onClick={handleClick}>Doklady</button>
+        <div className={openList ? 'seznam' : 'seznam--closed'}>
+          <ul>
+            {polozky.map((polozka) => (
+              <li key={polozka.id}>
+                {polozka.nazev}
+                <button
+                  onClick={() =>
+                    db.collection('more-doklady').doc(polozka.id).delete()
+                  }
+                >
+                  Odstranit
+                </button>
+              </li>
+            ))}
+          </ul>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              db.collection('more-doklady').add({
+                nazev: pridaniPolozky,
+                datumVytvoreni: firebase.firestore.FieldValue.serverTimestamp(),
+              });
+              setPridaniPolozky('');
+            }}
+          >
+            <label>
+              Přidat položku:
+              <input
+                value={pridaniPolozky}
+                onChange={(e) => setPridaniPolozky(e.target.value)}
+              ></input>
+            </label>
+            <button>Přidat</button>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/Sablony/MElektronika.jsx
+++ src/Sablony/MElektronika.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../db.js';
+import firebase from 'firebase/app';
+import './style.css';
 
 const MElektronika = () => {
   const [polozky, setPolozky] = useState([]);
@@ -7,6 +9,7 @@ const MElektronika = () => {
   useEffect(() => {
     const uklidPoSobe = db
       .collection('more-elektronika')
+      .orderBy('datumVytvoreni')
       .onSnapshot((snapshot) => {
         setPolozky(
           snapshot.docs.map((doc) => {
@@ -20,15 +23,51 @@ const MElektronika = () => {
     return uklidPoSobe;
   }, []);
 
+  const [pridaniPolozky, setPridaniPolozky] = useState('');
+  const [openList, setOpenList] = useState(false);
+  const handleClick = () => {
+    setOpenList(!openList);
+  };
+
   return (
     <>
-      <h3>Elektronika</h3>
       <div>
-        <ul>
-          {polozky.map((polozka) => (
-            <li key={polozka.nazev}>{polozka.nazev}</li>
-          ))}
-        </ul>
+        <button onClick={handleClick}>Elektronika</button>
+        <div className={openList ? 'seznam' : 'seznam--closed'}>
+          <ul>
+            {polozky.map((polozka) => (
+              <li key={polozka.id}>
+                {polozka.nazev}
+                <button
+                  onClick={() =>
+                    db.collection('more-elektronika').doc(polozka.id).delete()
+                  }
+                >
+                  Odstranit
+                </button>
+              </li>
+            ))}
+          </ul>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              db.collection('more-elektronika').add({
+                nazev: pridaniPolozky,
+                datumVytvoreni: firebase.firestore.FieldValue.serverTimestamp(),
+              });
+              setPridaniPolozky('');
+            }}
+          >
+            <label>
+              Přidat položku:
+              <input
+                value={pridaniPolozky}
+                onChange={(e) => setPridaniPolozky(e.target.value)}
+              ></input>
+            </label>
+            <button>Přidat</button>
+          </form>
+        </div>
       </div>
     </>
   );
