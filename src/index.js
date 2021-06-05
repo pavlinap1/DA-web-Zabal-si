5b3e33136f6350060c1b3f655ba252f3a5f5062a
Pavlina Petyrkova <petyrkovapavlina@seznam.cz>
2021-06-05 14:25:25 +0200

    Seznam-položky

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyArm9CcIAsx3zLhpFhN4IU2PnQQ4bO6bF8',
-  authDomain: 'zabal-si.firebaseapp.com',
-  projectId: 'zabal-si',
-  storageBucket: 'zabal-si.appspot.com',
-  messagingSenderId: '637072304106',
-  appId: '1:637072304106:web:5dcac3fbd6a32c99aa7493',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
+const seznam = [
+  {
+    name: 'Čundr',
+    obleceni: [],
+  },
+];
