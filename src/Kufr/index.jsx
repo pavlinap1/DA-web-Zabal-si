0ed91f7bbab100d3b1f4367d8de294cbcf13c3d7
Pavlina Petyrkova <petyrkovapavlina@seznam.cz>
2021-06-05 17:49:52 +0200

    Neco

--- src/Kufr/index.jsx
+++ src/Kufr/index.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
+import { seznam } from '../seznam';
 import './style.css';
 
-const Sekce = ({ jmeno, data }) => (
-  <>
-    <div>
-      <h2 className="nazevSekce">{jmeno}</h2>
-      <ul>
-        {data.map((polozka, index) => (
-          <li key={'il' + index}> {polozka}</li>
-        ))}
-      </ul>
-    </div>
-  </>
-);
+const Sekce = ({ jmeno, data }) => {
+  const [novaPolozka, setNovaPolozka] = useState('');
+  const [seznamPolozek, setSeznamPolozek] = useState(data);
+  const handleClickPolozka = () => {
+    const novySeznamPolozek = [novaPolozka].concat(seznamPolozek);
+    setSeznamPolozek(novySeznamPolozek);
+  };
+
+  return (
+    <>
+      <div>
+        <h2 className="nazevSekce">{jmeno}</h2>
+        <ul>
+          {seznamPolozek.map((polozka, index) => (
+            <li key={'il' + index}> {polozka}</li>
+          ))}
+          <input
+            onChange={(e) => setNovaPolozka(e.target.value)}
+            type="text"
+          ></input>
+          <button onClick={handleClickPolozka}>Přidat položku</button>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 const Polozka = ({ typPolozka }) => (
   <>
