db8b00244b8e5b752be511023a0df51488037243
Pavlina Petyrkova <petyrkovapavlina@seznam.cz>
2021-06-08 21:52:24 +0200

    Odstranení kurzovního lístku-zbytečné

--- src/Vizitka/index.jsx
+++ src/Vizitka/index.jsx
@@ -3,56 +3,6 @@ import { seznam } from '../seznam';
 import Kufr from '../Kufr';
 import './style.css';
 import { useHistory } from 'react-router-dom';
-const Mena = ({ select }) => {
-  const [from, setFrom] = useState('');
-  useEffect(() => {
-    fetch(`https://api.frankfurter.app/latest?from=${select}&to=CZK`)
-      .then((resp) => resp.json())
-      .then((json) => setFrom(json.rates.CZK));
-  }, [select]);
-  return (
-    <div className="rate">
-      <div className="rate__currency">1 {select}</div>
-      <div>=</div>
-      <div className="rate__value">{from} CZK</div>
-    </div>
-  );
-};
-
-const KurzovniListek = () => {
-  const [kurz, setKurz] = useState('USD');
-  const zmenaKurzu = (e) => {
-    setKurz(e.target.value);
-  };
-  return (
-    <>
-      <div>
-        <h1>Kurzovní lístek</h1>
-        <form>
-          <label>
-            <select value={kurz} onChange={zmenaKurzu}>
-              <option value="USD">Americký dolar</option>
-              <option value="EUR">Euro</option>
-              <option value="GBP">Britská libra</option>
-              <option value="BGN">Bulharský lev</option>
-              <option value="DKK">Dánský koruna</option>
-              <option value="HRK">Chorvatský kuna</option>
-              <option value="ISK">Islandská koruna</option>
-              <option value="CHF">Švýcarský frank</option>
-              <option value="HUF">Maďarský forint</option>
-              <option value="TRY">Turecká lira</option>
-              <option value="SEK">Švédská koruna</option>
-              <option value="RUB">Ruský rubl</option>
-              <option value="PLN">Polský zlotý</option>
-              <option value="NOK">Norská koruna</option>
-            </select>
-          </label>
-        </form>
-        <Mena select={kurz} />
-      </div>
-    </>
-  );
-};
 
 const Vizitka = ({ dataToVizitka, onTiskOK }) => {
   let history = useHistory();
@@ -108,7 +58,6 @@ const Vizitka = ({ dataToVizitka, onTiskOK }) => {
         </p>
         <p> Počet zavazadel {dataToVizitka.pocetZavazadel} </p>
 
-        <KurzovniListek />
         {kufrSeznam}
         <button onClick={handleClickTisk}>Mám nachystáno, chci tisknout</button>
       </div>
